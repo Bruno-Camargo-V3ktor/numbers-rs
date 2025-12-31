bfd8650c10f592eb978b0cd37e3eff83fce1e007
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-30 21:28:54 -0300

    feat: create error mensagens and validates

--- src/pages/main/form.rs
+++ src/pages/main/form.rs
@@ -13,16 +13,29 @@ pub fn FormContianer(props: FormContianerProps) -> Element {
     let at_input = use_signal(|| 100);
     let mut repetions = use_signal(|| true);
     let mut state = props.state.clone();
+    let mut msg_error = use_signal(|| Option::<String>::None);
 
     let on_submit_form = move |evt: Event<MouseData>| {
         evt.prevent_default();
-        let nums = rand_rang(
-            from_input(),
-            at_input(),
-            quantity_input() as usize,
-            !repetions(),
-        );
 
+        let quantity = quantity_input();
+        let from = from_input();
+        let at = at_input();
+        let repetions = repetions();
+
+        if quantity == 0 {
+            msg_error.set(Some(String::from(
+                "A quantidade minima de números tem que ser maior que 0",
+            )));
+            return;
+        }
+
+        if (at <= from) || (repetions && (at - from) < quantity) {
+            msg_error.set(Some(String::from("Intervalo de valores invalido")));
+            return;
+        }
+
+        let nums = rand_rang(from, at, quantity as usize, !repetions);
         let old_state = state().clone();
         state.set(StateNumbers {
             numbers: Some(nums),
@@ -48,6 +61,7 @@ pub fn FormContianer(props: FormContianerProps) -> Element {
                         max: 100,
                         name: "quantity",
                         signal: quantity_input.clone(),
+                        msg_error: msg_error.clone(),
                     }
 
                     InputNumber {
@@ -57,6 +71,8 @@ pub fn FormContianer(props: FormContianerProps) -> Element {
                         max: 1000,
                         name: "from",
                         signal: from_input.clone(),
+                        msg_error: msg_error.clone(),
+
                     }
 
                     InputNumber {
@@ -66,6 +82,7 @@ pub fn FormContianer(props: FormContianerProps) -> Element {
                         max: 1000,
                         name: "at",
                         signal: at_input.clone(),
+                        msg_error: msg_error.clone(),
                     }
                 }
                 input {
@@ -75,6 +92,10 @@ pub fn FormContianer(props: FormContianerProps) -> Element {
                     oninput: move |e| {
                         repetions.set(e.value().parse::<bool>().unwrap());
                     },
+                }
+
+                if let Some(msg) = msg_error() {
+                    p { class: "form-error", "{msg}" }
                 }
 
                 button { class: "btn-random", onclick: on_submit_form,
@@ -96,11 +117,18 @@ struct InputNumberProps {
     pub min: u16,
     pub max: u16,
     pub signal: Signal<u16>,
+    pub msg_error: Signal<Option<String>>,
 }
 
 #[component]
 fn InputNumber(props: InputNumberProps) -> Element {
     let mut signal = props.signal.clone();
+    let mut msg_error = props.msg_error.clone();
+
+    let on_changed = move |evt: Event<FormData>| {
+        signal.set(evt.value().parse::<u16>().unwrap_or(0));
+        msg_error.set(None);
+    };
 
     rsx! {
         label { class: "number-label", r#for: "{props.name}", "{props.title}" }
@@ -113,7 +141,7 @@ fn InputNumber(props: InputNumberProps) -> Element {
             max: "{props.max}",
             value: "{props.default}",
 
-            oninput: move |evt| signal.set(evt.value().parse::<u16>().unwrap()),
+            oninput: on_changed,
         }
     }
 }
